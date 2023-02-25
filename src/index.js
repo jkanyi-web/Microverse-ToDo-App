9d796916cb28dc028c161e0ac79473daade85339
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-02-25 14:56:00 +0300

    Fix code for better functionality and work on review requirements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-unused-vars */
 /** @format */
+
 import './styles/main.css';
 
 const taskBox = document.querySelector('.task-box');
@@ -25,10 +25,10 @@ const showTodo = (filter) => {
               <p class='${isCompleted}'>${todo.name}</p>
             </label>
             <div class="settings">
-              <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+              <i id="ellipsis${id}" class="uil uil-ellipsis-h"></i>
               <ul class="task-menu">
-                <li onclick="editTask(${id}, '${todo.name}')"><i class="uil uil-pen"></i>Edit</li>
-                <li onclick="deleteTask(${id})"><i class="uil uil-trash"></i>Delete</li>
+                <li id="${id}${todo.name}"><i class="uil uil-pen"></i>Edit</li>
+                <li id="${id}"><i class="uil uil-trash"></i>Delete</li>
               </ul>
             </div>
           </li>
@@ -37,33 +37,69 @@ const showTodo = (filter) => {
     });
   }
   taskBox.innerHTML = li || "<span>You don't have any tasks here yet</span>";
+  // eslint-disable-next-line no-use-before-define
+  addElliplistiners();
 };
 showTodo('all');
 
-const showMenu = (selectedTask) => {
-  // Getting task menu div
-  const taskMenu = selectedTask.parentElement.lastElementChild;
-  taskMenu.classList.add('show');
-  document.addEventListener('click', (e) => {
-    // Removing show class from the task Menu on document click
-    if (e.target.tagName !== 'I' || e.target !== selectedTask) {
-      taskMenu.classList.remove('show');
-    }
-  });
-};
+const showMe = document.querySelectorAll('.uil.uil-ellipsis-h');
+for (let x = 0; x < showMe.length; x += 1) {
+  showMe[x].onclick = () => {
+    const taskMenu = showMe[x].parentElement.lastElementChild;
+    taskMenu.classList.add('show');
+    document.addEventListener('click', (e) => {
+      // Removing show class from the task Menu on document click
+      if (e.target.tagName !== 'I' || e.target !== showMe[x]) {
+        taskMenu.classList.remove('show');
+      }
+    });
+  };
+}
 
-const editTask = (taskId, taskName) => {
-  editId = taskId;
-  isEditedTask = true;
-  taskInput.value = taskName;
-};
+function editTask() {
+  const editTas = document.querySelectorAll('.task-menu');
+  for (let i = 0; i < editTas.length; i += 1) {
+    // eslint-disable-next-line no-loop-func
+    editTas[i].firstElementChild.onclick = () => {
+      const tname = editTas[i].parentElement.parentElement.firstElementChild.textContent.trim();
+      editId = i;
+      isEditedTask = true;
+      taskInput.value = tname;
+    };
+  }
+}
+editTask();
 
-const deleteTask = (deleteId) => {
-  // Removing selected task from array/todos
-  todos.splice(deleteId, 1);
-  localStorage.setItem('todo-list', JSON.stringify(todos));
-  showTodo('all');
-};
+function deleteTask() {
+  const delTas = document.querySelectorAll('.task-menu');
+  for (let k = 0; k < delTas.length; k += 1) {
+    // eslint-disable-next-line no-loop-func
+    delTas[k].lastElementChild.onclick = () => {
+      todos.splice(k, 1);
+      localStorage.setItem('todo-list', JSON.stringify(todos));
+      showTodo('all');
+    };
+  }
+}
+deleteTask();
+
+function addElliplistiners() {
+  const showMe = document.querySelectorAll('.uil.uil-ellipsis-h');
+  for (let x = 0; x < showMe.length; x += 1) {
+    showMe[x].onclick = () => {
+      const taskMenu = showMe[x].parentElement.lastElementChild;
+      taskMenu.classList.add('show');
+      document.addEventListener('click', (e) => {
+        // Removing show class from the task Menu on document click
+        if (e.target.tagName !== 'I' || e.target !== showMe[x]) {
+          taskMenu.classList.remove('show');
+        }
+      });
+      editTask();
+      deleteTask();
+    };
+  }
+}
 
 clearAll.addEventListener('click', () => {
   // Removing all tasks from array/todos
@@ -72,6 +108,7 @@ clearAll.addEventListener('click', () => {
   showTodo('all');
 });
 
+// eslint-disable-next-line no-unused-vars
 const updateStatus = (selectedTask) => {
   // getting paragraph that contains task name
   const taskName = selectedTask.parentElement.lastElementChild;
@@ -106,4 +143,5 @@ taskInput.addEventListener('keyup', (e) => {
     localStorage.setItem('todo-list', JSON.stringify(todos));
     showTodo('all');
   }
+  addElliplistiners();
 });
